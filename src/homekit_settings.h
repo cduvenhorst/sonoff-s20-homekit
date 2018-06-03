d31a69b8ebf21f097af9d402935cf4e0c5dc34a4
Carsten Duvenhorst <carsten@duvenhorst.net>
2018-06-03 22:04:27 +0200

    Updated QR-Code

--- src/homekit_settings.h
+++ src/homekit_settings.h
@@ -1,6 +1,6 @@
 // Generated pairing information. Use "make homekitSettings" to generate.
 
-#define SETUP_CODE "122-34-782"
-#define SETUP_ID "BL2Q"
-// URI is "X-HM://0042PBUHQBL2Q"
+#define SETUP_CODE "108-82-559"
+#define SETUP_ID "KRU1"
+// URI is "X-HM://0042OIV3ZKRU1"
 
